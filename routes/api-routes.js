59afacd3b8c4a16c9c5b7edfb195513656c609b4
hngdng <65930682+hngdngng@users.noreply.github.com>
2020-10-10 23:39:23 -0400

    updated api routes for exercise and stats html

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,9 +2,40 @@ const router = require("express").Router();
 const Workout = require("../models/userWorkout.js");
 
 router.get("/api/workouts", (req, res) => {
-  console.log("api routing");
   Workout.find({})
-  .then( dbWorkout => {
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findOneAndUpdate({ _id: req.params.id},
+    {$push: { exercises: req.body } },
+    {new: true, runValidators: true })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+  .then(dbWorkout => {
     res.json(dbWorkout);
   })
   .catch(err => {
